48b71b90c4cfc7daea32d83235ec0fb303577f41
milan.zivanov <milan.zivanov@gmail.com>
2018-04-28 20:05:28 +0200

    slide menu

--- main.js
+++ main.js
@@ -26,85 +26,104 @@ for(let i of imageSf) {
 
 backdrop.addEventListener('click', modalClose);
 
+
+// slide menu
+
+var hamburgerButton = document.querySelector('.hamburger__button');
+var mobileNav = document.querySelector('.mobile');
+
+function openMobile() {
+    mobileNav.classList.add('open');
+}
+
+function closeMobile() {
+    mobileNav.classList.remove('open');
+}
+
+hamburgerButton.addEventListener('click', openMobile);
+mobileNav.addEventListener('click', closeMobile);
+
+
+
 // learning
 
 // import fetch from 'node-fetch';
 // or
 // const fetch = require('node-fetch');
 
-async function fetchAvatarUrl(userId) {
+// async function fetchAvatarUrl(userId) {
     
-    // async await
-    // const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
-    const response = await fetch(`http://localhost:3000/comments/${userId}`);
+//     // async await
+//     // const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+//     const response = await fetch(`http://localhost:3000/comments/${userId}`);
 
-    const data = await response.json();
-    console.log(data.body);
+//     const data = await response.json();
+//     console.log(data.body);
     
-    // response
-    data
-    return data.body
+//     // response
+//     data
+//     return data.body
 
-    // With Promise
-    // return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
-    //     .then(response => response.json())
-    //     .then(data => data.username);
-}
+//     // With Promise
+//     // return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+//     //     .then(response => response.json())
+//     //     .then(data => data.username);
+// }
 
-const result = fetchAvatarUrl(1);
-result
+// const result = fetchAvatarUrl(1);
+// result
 
-// map
-var animals = [
-    {name: 'Misko', vrsta: 'pas'},
-    {name: 'Mia', vrsta: 'macka'},
-    {name: 'Bucko', vrsta: 'pas'},
-    {name: 'Linda', vrsta: 'konj'}
-];
+// // map
+// var animals = [
+//     {name: 'Misko', vrsta: 'pas'},
+//     {name: 'Mia', vrsta: 'macka'},
+//     {name: 'Bucko', vrsta: 'pas'},
+//     {name: 'Linda', vrsta: 'konj'}
+// ];
 
-// var names = animals.map(function(animal) {
-//     return animal.name;
-// });
-var names = animals.map((animal) => animal.vrsta)
-console.log(names);
+// // var names = animals.map(function(animal) {
+// //     return animal.name;
+// // });
+// var names = animals.map((animal) => animal.vrsta)
+// console.log(names);
 
-var names = [];
-for(let i = 0; i < animals.length; i++) {
-    names.push(animals[i].name);
-}
-console.log(names);
+// var names = [];
+// for(let i = 0; i < animals.length; i++) {
+//     names.push(animals[i].name);
+// }
+// console.log(names);
 
 
-// TODO: promises by wes bos
+// // TODO: promises by wes bos
 
-function sleep(ms) {
-    return new Promise((resolve, reject) => {
-        if(ms <= 300) {
+// function sleep(ms) {
+//     return new Promise((resolve, reject) => {
+//         if(ms <= 300) {
 
-        }
-        setTimeout(() => resolve(`Slept for ${ms}`), ms)
-    });
-}
+//         }
+//         setTimeout(() => resolve(`Slept for ${ms}`), ms)
+//     });
+// }
 
-sleep(500)
-    .then((result) => {
-        console.log(result);
-        return sleep(1000);
-    })
-    .then((result) => {
-        console.log(result);
-        return sleep(1500);
-    })
-    .then((result) => {
-        console.log(result);
-        return sleep('i slept enough!!!');
-    })
+// sleep(500)
+//     .then((result) => {
+//         console.log(result);
+//         return sleep(1000);
+//     })
+//     .then((result) => {
+//         console.log(result);
+//         return sleep(1500);
+//     })
+//     .then((result) => {
+//         console.log(result);
+//         return sleep('i slept enough!!!');
+//     })
 
-    // async await
+//     // async await
 
-    async function go() {
-        await sleep(1000);
-        const responese = await sleep(800);
-        console.log(response);
-    }
+//     async function go() {
+//         await sleep(1000);
+//         const responese = await sleep(800);
+//         console.log(response);
+//     }
 
